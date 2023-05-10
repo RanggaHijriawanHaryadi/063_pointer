e1f6ff6b09e68845e095ed95d4c83adc284b003e
RanggaHijriawaHaryadi <rangghrydi04@gmail.com>
2023-05-10 16:44:57 +0700

    menggunakan perintah int main untuk menjalankan program

--- poniter/poniter.cpp
+++ poniter/poniter.cpp
@@ -3,11 +3,19 @@ using namespace std;
 
 class mahasiswa {
 public:
-	int bim;
+	int nim;
 	void showNim();                //deklarasi method
 
 };
 
 void mahasiswa::showNim() {        //implementasi method diluar class
 	cout << "No Induk = " << nim << endl;
+}
+
+int main()
+{
+	mahasiswa mhs{ 1 };    //object mhs
+	mhs.showNim();         // Member Acces Operator
+
+
 }
